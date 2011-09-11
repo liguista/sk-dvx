2a40c640321ed50faf0c3f83a1c7c741ac020c48
searayca2010 <searayca2010@431f9efb-ae1b-d958-54c3-abc7b3997a75>
2011-09-11 18:41:10 +0000

    Small DB changes to support JUnit testing.
    Needed to re-reverse the DB

--- DVX_App_Demo/src/org/ski/dvx/hibernate/Movie.java
+++ DVX_App_Demo/src/org/ski/dvx/hibernate/Movie.java
@@ -16,21 +16,22 @@ public class Movie extends AbstractMovie implements java.io.Serializable {
 
 	/** full constructor */
 	public Movie(Country country, Status status, Rating rating,
-			String movieName, String movieSbnNumber, Integer movieActive,
-			Integer movieReleased, String movieRatingTemp,
+			String movieName, String movieSbnNumber, Integer movieAuthorFk,
+			Integer movieActive, Integer movieReleased, String movieRatingTemp,
 			Integer movieLanguageMainFk, Integer movieRating,
 			String movieQuote, String movieDescription,
 			String movieExtraFeatures, Integer movieRuntime,
 			Integer movieBudget, Integer movieRevenue, String movieWebpage,
-			Timestamp created, Timestamp modified, Set movieChapters,
-			Set transactionLogs, Set posters, Set reviews, Set movieTrailers,
-			Set movieMenus, Set movieCasts, Set descriptions) {
-		super(country, status, rating, movieName, movieSbnNumber, movieActive,
-				movieReleased, movieRatingTemp, movieLanguageMainFk,
-				movieRating, movieQuote, movieDescription, movieExtraFeatures,
-				movieRuntime, movieBudget, movieRevenue, movieWebpage, created,
-				modified, movieChapters, transactionLogs, posters, reviews,
-				movieTrailers, movieMenus, movieCasts, descriptions);
+			Integer moviePathFk, Timestamp created, Timestamp modified,
+			Set movieChapters, Set transactionLogs, Set posters, Set reviews,
+			Set movieTrailers, Set movieMenus, Set movieCasts, Set descriptions) {
+		super(country, status, rating, movieName, movieSbnNumber,
+				movieAuthorFk, movieActive, movieReleased, movieRatingTemp,
+				movieLanguageMainFk, movieRating, movieQuote, movieDescription,
+				movieExtraFeatures, movieRuntime, movieBudget, movieRevenue,
+				movieWebpage, moviePathFk, created, modified, movieChapters,
+				transactionLogs, posters, reviews, movieTrailers, movieMenus,
+				movieCasts, descriptions);
 	}
 
 }
