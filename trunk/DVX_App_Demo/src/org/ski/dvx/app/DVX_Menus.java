4916ef9ad2cbde23ff1c0fa897436fa75fcc7aab
searayca2010 <searayca2010@431f9efb-ae1b-d958-54c3-abc7b3997a75>
2011-09-11 22:27:55 +0000

    Added Copyright Headers.
    Fixed some database issues:
     All user type when filtering.  It was adding a new user becuase the All user was 'special'
    Added All user type to the constants.

--- trunk/DVX_App_Demo/src/org/ski/dvx/app/DVX_Menus.java
+++ trunk/DVX_App_Demo/src/org/ski/dvx/app/DVX_Menus.java
@@ -1,3 +1,35 @@
+/*
+	Project:		Windows DVX Player/Editor
+	
+	File Name:		
+
+	Designer:		Josh Miele
+
+	Author:			Greg Ames
+
+	Contributors:	Owen Edwards
+					
+	Created:		September 10, 2011
+	
+	Copyright:	 	The Smith-Kettlewell Eye Research Institute
+					© 2011
+					All Rights Reserved
+					
+					http://www.ski.org/
+					
+					The Smith-Kettlewell Eye Research Institute
+					2318 Fillmore Street 
+					San Francisco, CA  94115 
+					415-345-2000  
+				 
+	Notice:			Parts of this project are based on Open Source 
+					and/or Public Domain Code. Please use good judgement 
+					if you include any of this project in your project.
+					
+	Contact:		Josh Miele
+					jam@ski.org
+*/
+
 /**
  * 
  */
