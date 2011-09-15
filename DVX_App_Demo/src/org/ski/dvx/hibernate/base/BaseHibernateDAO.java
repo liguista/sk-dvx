db097d956957e6f6d720e8ce10912244d5c7b53d
searayca2010 <searayca2010@431f9efb-ae1b-d958-54c3-abc7b3997a75>
2011-09-15 02:18:30 +0000

    Moved to make reversing easier

---
+++ DVX_App_Demo/src/org/ski/dvx/hibernate/base/BaseHibernateDAO.java
@@ -0,0 +1,16 @@
+package org.ski.dvx.hibernate.base;
+
+import org.hibernate.Session;
+
+
+/**
+ * Data access object (DAO) for domain model
+ * @author MyEclipse Persistence Tools
+ */
+public class BaseHibernateDAO implements IBaseHibernateDAO {
+	
+	public Session getSession() {
+		return HibernateSessionFactory.getSession();
+	}
+	
+}
